b3e615dfea5a95d81e85047efbcc09aad34c0ab0
RLC <robz.l.c.77@gmail.com>
2019-02-13 20:20:50 -0600

    front end auth for pages

--- finance101/src/App.js
+++ finance101/src/App.js
@@ -1,23 +1,62 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom"
+import {
+  BrowserRouter as Router, Route, Link,
+  Redirect
+} from "react-router-dom"
 import './App.css';
 import Tool from "../src/pages/tool"
 import NoMatch from "../src/pages/NoMatch"
 import Login from "../src/pages/Login"
 import Register from "../src/pages/Register/Register"
+
+
+const fakeAuth = {
+  isAuthenticated: false,
+  authenticate(cb) {
+    this.isAuthenticated = true
+    setTimeout(cb, 100)
+  },
+  signout(cb) {
+    this.isAuthenticated = false
+    setTimeout(cb, 100)
+  }
+}
+const Public = () => <h3>Public</h3>
+const Protected = () => <h3>Protected</h3>
+
+const PrivateRoute = ({ component: Component, ...rest }) => (
+  <Route {...rest} render={(props) => (
+    fakeAuth.isAuthenticated === true
+      ? <Component {...props} />
+      : <Redirect to='/Login' />
+  )} />
+)
+
+
+
+
+
+
+
 class App extends Component {
+
+  state = {
+    username: "sardoSardinas",
+    password: "1234",
+  };
+
   render() {
     return (
       <Router>
-        <div >
+        <div>
+          <ul>
+            <li><Link to='/public'>Public Page</Link></li>
+            <li><Link to='/Protected'>Protected Page</Link></li>
+          </ul>
 
-          <Switch>
-            <Route exact path="/tool" component={Tool} />
-            <Route exact path="/" component={Login} />
-            <Route exact path="/register" component={Register} />
-
-            <Route component={NoMatch} />
-          </Switch>
+          <Route path='/public' component={Public} />
+          <Route path='/' component={Login} />
+          <PrivateRoute path='/protected' component={Protected} />
         </div>
       </Router>
     );
--- finance101/src/pages/tool.js
+++ finance101/src/pages/tool.js
@@ -4,72 +4,11 @@ import { InputData } from "../components/InputData";
 import Navs from "../components/Navs";
 import { Dropdown, Button } from "react-bootstrap";
 import API from "../utils/API";
-import{
-    BrowserRouter as Router,
-    Route,
-    Link,
-    Redirect
-} from 'react-router-dom';
+
 //import { set } from "mongoose";
-
-const fakeAuth={
-    isAuthenticated: false,
-    authenticate(cb) {
-        this.isAuthenticated= true
-        setTimeout(cb, 100)
-    },
-    signout(cb) {
-        this.isAuthenticated= false
-        setTimeout(cb,100)
-    }
-}
-
-const Public = () => <h3>Public</h3>
-const Protected = () => <h3>Protected</h3>
-
-class Login extends React.Component{
-    render() {
-        return(
-            <div>
-                LOGIN
-            </div>
-        )
-    }
-}
-
-const PrivateRoute =({component: Component, ...rest}) => (
- <Route {...rest} render = {(props) => (
-     fakeAuth.isAuthenticated === true
-     ? <Component {...props}/>
-     : <Redirect to='/Login' />
- )}/>
-)
-
-class App extends React.Component {
-    render() {
-        return(
-            <Router>
-                <div>
-                    <ul>
-                        <li><Link to='/public'>Public Page</Link></li>
-                        <li><Link to='/Protected'>Protected Page</Link></li>
-                    </ul>
-
-                    <Route path='/public' component = {Public}/>
-                    <Route path='/Login' component = {Login}/>
-                    <PrivateRoute path ='/protected' component= {Protected}/>
-                </div>
-            </Router>
-        )
-    }
-}
 
 class Tool extends Component {
 
-    userState = {
-        username: "sardoSardinas",
-        password: "1234",
-    };
 
     state = {
         Months: ["January", "February", "March"],
@@ -117,7 +56,7 @@ class Tool extends Component {
         })
     }
 
-   
+
 
     render() {
 
